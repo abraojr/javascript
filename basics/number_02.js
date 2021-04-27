3ecd7dc0d6811335e0e694b6bda212941b7b859f
Abrão Astério Junior <abcorinthians@gmail.com>
2021-04-26 23:48:55 -0300

    some precautions using number

---
+++ basics/number_02.js
@@ -0,0 +1,9 @@
+console.log(7 / 0);  //OUTPUT: Infinity
+console.log("10.2" / 2);  //OUTPUT: 5.1
+console.log("10,2" / 2);  //OUTPUT: NaN
+console.log('3' + 2);  //OUTPUT: 32
+console.log('3' - 2);  //OUTPUT: 1
+console.log("Show" * 2);  //OUTPUT: NaN
+console.log(0.1 + 0.7);  //OUTPUT: 0.7999999999999999
+//console.log(10.toString()); -> Error
+console.log((10.345).toFixed(1)); //OUTPUT: 10.3
