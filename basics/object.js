065d2f42423f10d2747d24d9a296f9f0631c8fc4
Abrão Astério Junior <abcorinthians@gmail.com>
2021-04-27 03:22:37 -0300

    types in js: object

---
+++ basics/object.js
@@ -0,0 +1,13 @@
+const prod1 = {};
+prod1.name = "Iphone X";
+prod1.price = 4998.90;
+prod1['Discount'] = 0.40;
+
+console.log(prod1);
+
+const prod2 = {
+    prodName: "PS5",
+    prodPrice: 6500.0
+}
+
+console.log(prod2);
