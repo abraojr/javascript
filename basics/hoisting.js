9246be5a2c96b3e166703dd1398b282e485ff528
Abrão Astério Junior <abcorinthians@gmail.com>
2021-05-16 19:23:38 -0300

    understanding the concept of hoisting

---
+++ basics/hoisting.js
@@ -0,0 +1,7 @@
+console.log('a = ' + a); //OUTPUT: undefinded
+var a = 2;
+console.log('a = ' + a); //OUTPUT: 2
+
+// console.log('b = ' + b); Error -> Cannot acces 'b' before initialization
+let b = 4;
+console.log('b = ' + b);
