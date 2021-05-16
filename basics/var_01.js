8263614c6e1c0284d78eec11f88491379bf66026
Abrão Astério Junior <abcorinthians@gmail.com>
2021-05-16 18:20:21 -0300

    declaring variables with var

---
+++ basics/var_01.js
@@ -0,0 +1,16 @@
+{
+    {
+        {
+            var hello = "Hello";
+        }
+    }
+}
+console.log(hello);
+
+function test() {
+    var local = 123;
+    console.log(local);
+}
+test();
+
+// console.log(local); Error 
