8b2356e4a0fd8a3fcecba5ad78d6e7e023aba664
Abrão Astério Junior <abcorinthians@gmail.com>
2021-05-19 01:15:33 -0300

    example 1 of factory functions

---
+++ functions/factory_functions_01.js
@@ -0,0 +1,9 @@
+//factory is a function that returns an object
+//Simple factory
+function createPerson() {
+    return {
+        name: 'Abrão',
+        lastName: 'Junior'
+    }
+}
+console.log(createPerson());
