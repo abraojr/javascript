15c1914dc8ba9b60d3437a7cfc80006cce93e414
Abrão Astério Junior <abcorinthians@gmail.com>
2021-05-20 02:12:52 -0300

    Exercise 1: Function that returns basic math operations

---
+++ exercises/ex_001.js
@@ -0,0 +1,8 @@
+/*Create a function that given two values (passed as parameters) shows in the console the sum, subtraction,
+ multiplication and division of these values */
+
+operations = (a, b) => console.log('Sum:' + (a + b) + ', Subtraction:' + (a - b) + ', Multiplication:' + (a * b) + ', Division:' + (a / b));
+
+operations(2, 2);
+operations(4, 2);
+operations(5, 10);
