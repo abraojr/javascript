a045dd390947a9aaeea87f67832a4fa984ea8e3c
Abrão Astério Junior <abcorinthians@gmail.com>
2021-05-20 03:03:10 -0300

    Exercise 3: Exponentiation

---
+++ exercises/ex_003.js
@@ -0,0 +1,12 @@
+//Create a function that takes two parameters, base and exponent, and returns the base raised to the exponent.
+
+const exponentiation = (base, exponent) => console.log(base ** exponent);
+
+exponential(2, 3);
+exponential(2, 4);
+exponential(2, 5);
+exponential(2, 6);
+exponential(2, 7);
+exponential(2, 8);
+exponential(2, 9);
+exponential(2, 10);
