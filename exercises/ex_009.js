650cdd7b3977cfbeb1796bae0aeca6c748361e66
Abrão Astério Junior <abcorinthians@gmail.com>
2021-05-20 19:27:08 -0300

    Exercise 9: Divisible or not by 3

---
+++ exercises/ex_009.js
@@ -0,0 +1,7 @@
+//Create a function that checks if an integer passed as parameter is divisible by 3 and returns true or false.
+
+const isDividedByThree = value => value % 3 == 0 ? true : false;
+
+console.log(isDividedByThree(3));
+console.log(isDividedByThree(2))
+console.log(isDividedByThree(150));
