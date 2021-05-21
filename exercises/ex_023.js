5737ac93cfbc5c47d6cbb53daa24f0d96084fccb
Abrão Astério Junior <abcorinthians@gmail.com>
2021-05-21 01:26:43 -0300

    Exercise 23: Using while-repeat structure

---
+++ exercises/ex_023.js
@@ -0,0 +1,11 @@
+/*Create a program that prints the phrase " Hello World!" 11 times using a while-repeat structure.*/
+
+function helloWorld() {
+    let i = 0;
+    while (i < 11) {
+        console.log("Hello World");
+        i++;
+    }
+}
+
+helloWorld();
