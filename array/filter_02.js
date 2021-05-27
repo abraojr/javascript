de116d9180906e01e3b58f56dbab7282f1f5bdc8
Abrão Astério Junior <abcorinthians@gmail.com>
2021-05-26 22:37:32 -0300

    implementing filter manually

---
+++ array/filter_02.js
@@ -0,0 +1,21 @@
+Array.prototype.filter2 = function (callback) {
+    const newArray = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            newArray.push(this[i]);
+        }
+    }
+    return newArray;
+}
+
+const products = [
+    { name: 'Notebook', price: 2499, fragile: true },
+    { name: 'iPad Pro', price: 4199, fragile: true },
+    { name: 'Glass cup', price: 12.49, fragile: true },
+    { name: 'Plastic cup', price: 18.99, fragile: false },
+];
+
+const isExpensive = product => product.price >= 500;
+const isFragile = product => product.fragile;
+
+console.log(products.filter2(isExpensive).filter2(isFragile));
