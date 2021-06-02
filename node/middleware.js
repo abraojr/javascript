5628789997c8f28102396b290a33571dfe1ea246
Abrão Astério Junior <abcorinthians@gmail.com>
2021-06-02 03:42:22 -0300

    implementing the middleware pattern

---
+++ node/middleware.js
@@ -0,0 +1,24 @@
+// middleware pattern (chain of responsibility)
+const step1 = (context, next) => {
+    context.value1 = 'mid1';
+    next();
+}
+
+const step2 = (context, next) => {
+    context.value2 = 'mid2';
+    next();
+}
+
+const step3 = context => context.value3 = 'mid3';
+
+const exec = (context, ...middlewares) => {
+    const execStep = index => {
+        middlewares && index < middlewares.length &&
+            middlewares[index](context, () => execStep(index + 1))
+    };
+    execStep(0);
+};
+
+const context = {};
+exec(context, step1, step2, step3);
+console.log(context);
